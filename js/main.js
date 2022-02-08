86b9e6f544ba0f8586554ff0700012460b6b8fc3
Franciscof94 <91231976+Franciscof94@users.noreply.github.com>
2022-02-08 12:19:34 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,7 +54,72 @@ function listCategories() {
   });
 }
 
+function listProducts(idCategory) {
+  const productsNode = document.getElementById("products");
 
+  productsNode.innerHTML = "";
+  let lista = document.createElement("div");
+  const productsCategories = products.filter(
+    (element) => element.category === idCategory
+  );
+  for (var i = 0; i < productsCategories.length; i++) {
+    let product = productsCategories[i];
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.innerHTML = `<img src=${product.img} class="imgProduct"/>
+                     <div class="itemName">${product.mark}</div>
+                     <div class="itemPrice">$${product.price}</div>
+                     `;
+    const subtractButton = document.createElement("button");
+    const addButton = document.createElement("button");
+    subtractButton.classList.add("btnAddSubtract");
+    addButton.classList.add("btnAddSubtract");
+
+    let countProduct = 0;
+    let count = document.createElement("div");
+    count.classList.add("count");
+    count.textContent = countProduct;
+
+    const containerBtnCount = document.createElement("div");
+    containerBtnCount.classList.add("containerBtnCount");
+
+    let button = document.createElement("button");
+    button.classList.add("addButton");
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+
+    addButton.innerHTML = "+";
+    subtractButton.innerHTML = "-";
+
+    addButton.addEventListener("click", () => {
+      countProduct = ++countProduct;
+      count.textContent = countProduct;
+    });
+
+    subtractButton.addEventListener("click", () => {
+      if (countProduct > 0) {
+        countProduct = --countProduct;
+        count.textContent = countProduct;
+      }
+    });
+
+    item.append(buttonContainer);
+    containerBtnCount.appendChild(subtractButton);
+    containerBtnCount.appendChild(count);
+    containerBtnCount.appendChild(addButton);
+    buttonContainer.appendChild(containerBtnCount);
+    buttonContainer.appendChild(button);
+    productsNode.appendChild(item);
+
+    $(button).click(function () {
+      console.log(countProduct + 3);
+      addProduct(product, countProduct);
+    });
+
+    button.innerHTML = "AGREGAR";
+  }
+}
 
 function updateCart() {
   cartNode.innerHTML = "";
